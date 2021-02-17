3446e2ec8577143b650180d7d0b95d610338c7e2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-17 17:21:17 +0100

    sm6150-common: Guard qti thermal 2.0 service hals
    * Its crashes on alioth but fine on other devices
    
     * Crash:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
    
    sm8250-common: Build QTI thermal HIDL
    
    Change-Id: Id8d468198cef11a67cd40a93a3c50de7185bec43
    Co-Authored-By: Zinadin Zidan <zidan.roking@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -94,6 +94,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+# Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+endif
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
