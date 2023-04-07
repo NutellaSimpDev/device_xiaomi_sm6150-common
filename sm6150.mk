baf7138050fc5c8d4c66a51c3293036a8ae157fd
Eric Laurent <elaurent@google.com>
2023-04-07 22:47:05 +0800

    sm6150-common: audio: Configuration for spatial audio
    Update audio configuration to enable spatial audio:
    - import spatial audio library from cheetah TQ2A.230305.008.C1
    - list spatializer effect in audio effect configuration file
    - update audio policy configuration files to declare spatializer output
    - declare immersive end point in audio platform configuration
    
    Bug: 237677573
    Test: spatial audio playback
    Change-Id: Ic9c32d813a19ba2789425e12ee9016d53ab30678
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -63,7 +63,12 @@ PRODUCT_PACKAGES += \
     libaudio-resampler
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc) \
+    $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(LOCAL_PATH)/configs/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/bluetooth_audio_policy_configuration_7_0.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
+    $(LOCAL_PATH)/configs/audio/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
